6c0253b5185638a9fbcb753fbb6428ba63b64719
Felipe300 <caracoloro@gmail.com>
2022-01-27 17:07:56 -0300

    Add express and morgan

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World')
+import express from 'express'
+import morgan from 'morgan'
+
+const app = express()
+const PORT = Number(process.env.PORT ?? 5000)
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+  res.json({ hello: 'World!' })
+})
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server listening on port ${PORT}`)
+})
